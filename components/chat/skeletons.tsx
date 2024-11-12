a44c2b3b81891fd2af639bfc2d56e9a2eef882a9
laguagu <akuleskinen@hotmail.com>
2024-11-12 15:18:27 +0200

    Style skeleton

--- components/chat/skeletons.tsx
+++ components/chat/skeletons.tsx
@@ -3,7 +3,7 @@ import { ThreeDots } from "react-loader-spinner";
 
 export const SmoothLoader = () => (
   <div className="flex items-center justify-center">
-    <div className="mr-2 w-5 h-5 border-2 border-gray-300 border-t-gray-600 rounded-full animate-spin"></div>
+    <div className="mr-2 w-5 h-5 border-2 border-gray-100 border-t-gray-600 rounded-full animate-spin"></div>
   </div>
 );
 
@@ -12,8 +12,8 @@ export function WoodnotesImageProcessingSkeleton() {
     <div className="flex flex-col items-center justify-center space-y-4">
       <Skeleton className="h-64 w-64 sm:h-80 sm:w-80 md:h-96 md:w-96 rounded-lg bg-gray-200" />
       <div className="flex items-center space-x-2">
-        <Skeleton className="h-4 w-4 rounded-full bg-gray-300" />
-        <Skeleton className="h-4 w-32 bg-gray-300" />
+        <Skeleton className="h-4 w-4 rounded-full bg-gray-100" />
+        <Skeleton className="h-4 w-32 bg-gray-100" />
       </div>
       <div className="flex items-center justify-center">
         <span className="text-lg font-medium text-gray-600 mr-2">
@@ -29,10 +29,10 @@ export function CameraSkeleton() {
   return (
     <div>
       <div className="flex flex-col space-y-8">
-        <Skeleton className="h-72 min-w-[350px] sm:h-72 md:h-80 md:w-[520px] lg:h-96 lg:w-[620px] rounded-xl bg-gray-300 shadow-md" />
+        <Skeleton className="h-72 min-w-[350px] sm:h-72 md:h-80 md:w-[520px] rounded-xl bg-gray-100 shadow-md" />
         <div className="flex space-x-4 justify-center">
-          <Skeleton className="h-8 w-32 sm:w-32 md:w-40 lg:w-48 bg-slate-700" />
-          <Skeleton className="h-8 w-28 sm:w-28 md:w-36 lg:w-44 bg-slate-700" />
+          <Skeleton className="h-8 w-32 sm:w-32 md:w-40 lg:w-48 bg-slate-200" />
+          <Skeleton className="h-8 w-28 sm:w-28 md:w-36 lg:w-44 bg-slate-200" />
         </div>
       </div>
     </div>
